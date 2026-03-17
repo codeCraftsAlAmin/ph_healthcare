fa3bd8b79f5eb3a22c7e685a42413179f1a82700
Code Crafts <codecraftsman12@gmail.com>
2026-03-17 23:55:36 +0600

    Completed backend code

--- healtcare_backend/src/app/middleware/globalError.ts
+++ healtcare_backend/src/app/middleware/globalError.ts
@@ -6,6 +6,14 @@ import z from "zod";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import AppError from "../errorHelpers/AppError";
 import { deleteUploadFilesFromGlobalErrHandler } from "../utils/deleteUploadFilesFromGlobalErrHandler";
+import {
+  handlePrismaClientInitializationError,
+  handlePrismaClientKnownRequestError,
+  handlePrismaClientPanicError,
+  handlePrismaClientUnknownRequestError,
+  handlePrismaClientValidationError,
+} from "../errorHelpers/handlePrizmaErrors";
+import { Prisma } from "../../generated/prisma/client";
 
 export const globalError = async (
   err: any,
@@ -25,7 +33,37 @@ export const globalError = async (
   let statusCode: number = status.INTERNAL_SERVER_ERROR;
   let errMessage: string = "Internal server error";
 
-  if (err instanceof z.ZodError) {
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    // prisma known request error
+    const simplifiedError = handlePrismaClientKnownRequestError(err);
+    statusCode = simplifiedError.statusCode as number;
+    errMessage = simplifiedError.message;
+    errorSource = [...simplifiedError.errorSource];
+  } else if (err instanceof Prisma.PrismaClientRustPanicError) {
+    // prisma rust panic error
+    const simplifiedError = handlePrismaClientPanicError(err);
+    statusCode = simplifiedError.statusCode as number;
+    errMessage = simplifiedError.message;
+    errorSource = [...simplifiedError.errorSource];
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
+    // prisma initialization error
+    const simplifiedError = handlePrismaClientInitializationError(err);
+    statusCode = simplifiedError.statusCode as number;
+    errMessage = simplifiedError.message;
+    errorSource = [...simplifiedError.errorSource];
+  } else if (err instanceof Prisma.PrismaClientValidationError) {
+    // prisma validation error
+    const simplifiedError = handlePrismaClientValidationError(err);
+    statusCode = simplifiedError.statusCode as number;
+    errMessage = simplifiedError.message;
+    errorSource = [...simplifiedError.errorSource];
+  } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    // prisma unknown request error
+    const simplifiedError = handlePrismaClientUnknownRequestError(err);
+    statusCode = simplifiedError.statusCode as number;
+    errMessage = simplifiedError.message;
+    errorSource = [...simplifiedError.errorSource];
+  } else if (err instanceof z.ZodError) {
     // throw zod error
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError.statusCode as number;
--- healtcare_backend/src/server.ts
+++ healtcare_backend/src/server.ts
@@ -1,18 +1,70 @@
 import app from "./app";
 import { envVars } from "./app/config/env";
 import { seedSuperAdmin } from "./app/utils/seed";
+import { Server } from "http";
 
 const port = envVars.PORT;
+let server: Server;
 
 const bootsTrap = async () => {
   try {
     await seedSuperAdmin();
-    app.listen(port, () => {
+    server = app.listen(port, () => {
       console.log(`Server is running on ~ 🚀 http://localhost:${port}`);
     });
   } catch (error) {
     console.log("Failed to start server ~ ❌", error);
   }
 };
+
+// SIGTERM signal handler
+process.on("SIGTERM", () => {
+  console.log("Server is closing ~ 🚨");
+  if (server) {
+    server.close(() => {
+      console.log("Server closed ~ 🚨");
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// SIGINT signal handler
+process.on("SIGINT", () => {
+  console.log("Server is closing ~ 🚨");
+  if (server) {
+    server.close(() => {
+      console.log("Server closed ~ 🚨");
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//uncaught exception handler
+process.on("uncaughtException", (error) => {
+  console.log("Uncaught Exception ~ ❌", error);
+  if (server) {
+    server.close(() => {
+      console.log("Server closed ~ 🚨");
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+//unhandled rejection handler
+process.on("unhandledRejection", (reason) => {
+  console.log("Unhandled Rejection ~ ❌", reason);
+  if (server) {
+    server.close(() => {
+      console.log("Server closed ~ 🚨");
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
 
 bootsTrap();
